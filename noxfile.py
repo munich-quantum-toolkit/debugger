215a080e076c8c03e03ba409ebc62aaad6365a42
Daniel Haag <121057143+denialhaag@users.noreply.github.com>
2025-09-25 20:36:57 +0200

    Add workflow to run Qiskit upstream tests

--- noxfile.py
+++ noxfile.py
@@ -130,6 +130,18 @@ def minimums(session: nox.Session) -> None:
         session.run("uv", "tree", "--frozen", env=env)
 
 
+@nox.session(reuse_venv=True, venv_backend="uv", python=PYTHON_ALL_VERSIONS)
+def qiskit(session: nox.Session) -> None:
+    """Tests against the latest version of Qiskit."""
+    with preserve_lockfile():
+        _run_tests(
+            session,
+            extra_command=["uv", "pip", "install", "qiskit[qasm3-import] @ git+https://github.com/Qiskit/qiskit.git"],
+        )
+        env = {"UV_PROJECT_ENVIRONMENT": session.virtualenv.location}
+        session.run("uv", "pip", "show", "qiskit", env=env)
+
+
 @nox.session(reuse_venv=True)
 def docs(session: nox.Session) -> None:
     """Build the docs. Use "--non-interactive" to avoid serving. Pass "-b linkcheck" to check links."""
