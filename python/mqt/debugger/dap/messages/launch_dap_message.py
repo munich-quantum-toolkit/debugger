c1eaccdfc834c63f1069aa47bef4ee7d441519b4
gitAccountS <sven.je.schmitt@web.de>
2026-01-21 17:32:43 +0100

    improvement launch dap

--- python/mqt/debugger/dap/messages/launch_dap_message.py
+++ python/mqt/debugger/dap/messages/launch_dap_message.py
@@ -80,8 +80,10 @@ class LaunchDAPMessage(DAPMessage):
         if parsed_successfully and not self.stop_on_entry:
             server.simulation_state.run_simulation()
         if not parsed_successfully:
-            with contextlib.suppress(RuntimeError):
+            try:
                 server.simulation_state.reset_simulation()
+            except RuntimeError:
+                pass
         return {
             "type": "response",
             "request_seq": self.sequence_number,
